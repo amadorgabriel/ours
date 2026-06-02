11f3af32d0fc776496b88ac19c799e361844bb11
amadorgabriel <amadorgabriel.dev@gmail.com>
2026-06-02 12:16:46 -0300

    feat: update .gitignore

--- client/src/i18n/routing.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { routing } from '@/i18n/routing';
-
-describe('i18n routing', () => {
-  it('default locale is pt-BR', () => {
-    expect(routing.defaultLocale).toBe('pt-BR');
-  });
-});
